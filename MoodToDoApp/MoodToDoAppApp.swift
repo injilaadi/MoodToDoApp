c9762fe0b025221f7eb0f08851f47de335d68a26
Injila Adil <injilaadil@gmail.com>
2023-07-31 14:25:13 -0700

    Initial Commit

---
+++ MoodToDoApp/MoodToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodToDoAppApp.swift
+//  MoodToDoApp
+//
+//  Created by Injila Adil on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct MoodToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
