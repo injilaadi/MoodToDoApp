78b013dc3aa1a79ad34fb8a2c369d0609f349d68
Tugs-Oyun Enkh-Amgalan <togsoyun@gmail.com>
2023-08-02 09:02:13 -0700

    positive message generator

--- MoodToDoApp/ContentView.swift
+++ MoodToDoApp/ContentView.swift
@@ -8,42 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    var positiveMessages = [ "You are quite ravishing today!", "Drink water!"]
+    @State var displayedMessage = "You are quite ravishing today!"
     
     var body: some View {
         let blueColor = Color(red: 191/255, green: 215/255, blue: 237/255)
-        NavigationStack{
+        ZStack {
             blueColor
                 .ignoresSafeArea()
-            VStack{
+            
+            VStack (alignment:.center){
+                Text("Mood To Do")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
                 
-            }
-                .toolbar{
-                    ToolbarItemGroup(placement: .status){
-                        NavigationLink(destination: ContentView()) {
-                            Image("HomeIcon")
-                                .resizable(resizingMode: .stretch)
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 50.0)
-                               
-                        }
-                        NavigationLink(destination: SchedulePage()) {
-                                Image("CalendarIcon")
-                                    .resizable(resizingMode: .stretch)
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 50.0)
-                                    
-                        }
-                        NavigationLink(destination: MoodPage()) {
-                                Image("SmileIcon")
-                                    .resizable(resizingMode: .stretch)
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 50.0)
-                                    
-                        }
+                // positive message generator
+                Text(displayedMessage)
+                Button("") {
+                    displayedMessage = positiveMessages [Int.random(in: 0..<positiveMessages.count)]
+                }
+                .font(.title2)
+                .buttonStyle(.borderedProminent)
+                .tint(Color (red: 50/255, green: 80/255, blue: 50/255))
+                
+                NavigationStack{
+                    blueColor
+                        .ignoresSafeArea()
+                    VStack{
+                        
                     }
+                        .toolbar{
+                            ToolbarItemGroup(placement: .status){
+                                NavigationLink(destination: ContentView()) {
+                                    Image("HomeIcon")
+                                        .resizable(resizingMode: .stretch)
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 50.0)
+                                       
+                                }
+                                NavigationLink(destination: SchedulePage()) {
+                                        Image("CalendarIcon")
+                                            .resizable(resizingMode: .stretch)
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 50.0)
+                                            
+                                }
+                                NavigationLink(destination: MoodPage()) {
+                                        Image("SmileIcon")
+                                            .resizable(resizingMode: .stretch)
+                                            .aspectRatio(contentMode: .fit)
+                                            .frame(width: 50.0)
+                                            
+                                }
+                            }
+                            
+                        }
                     
                 }
-            
+            }
         }
     }
 }
